d62e540030057eb0709b8c10831c78c20c6d4553
Anubhav Kumar <66882903+findanubhav420@users.noreply.github.com>
2023-07-12 10:56:52 +0530

    fix: cors errors

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use(router);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.get('/', (req, res) => res.send('Authentication server is running!'));
+
 
 const PORT = 4000;
 
